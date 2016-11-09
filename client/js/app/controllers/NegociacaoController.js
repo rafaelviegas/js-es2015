c374de584cf9cf8aac6835462c1f7d25725e7b76
Rafael Viegas <rafael.viegas@arpagus.com.br>
2016-11-09 17:04:28 -0200

    4. Lidar com data é trabalhoso? Chame um ajudante!

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,20 +5,27 @@
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
 
     }
 
     adiciona(event){
-        event.preventDefault();
-        let data = new Date(...this._inputData.value
-                            .split('-')
-                            .map((item,index) => item - index % 2));
-        let negociacao = new Negociacao(
-            data,
+        event.preventDefault();   
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade,
-            this._inputValor
-        );
-        console.log(negociacao);
+            this._inputValor);
+    }
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }       
 }
 
