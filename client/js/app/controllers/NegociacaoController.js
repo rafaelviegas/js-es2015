d858aeae6e1259b43c4f59573e2c58d7642b73f8
Rafael Viegas <rafael.viegas@arpagus.com.br>
2016-11-10 11:37:58 -0200

    5. Temos o modelo, mas e a view?

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,19 +7,22 @@
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
 
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event){
         event.preventDefault();   
         this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
         this._limpaFormulario();
     }
 
     _criaNegociacao(){
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade,
-            this._inputValor);
+            this._inputQuantidade.value,
+            this._inputValor.value);
     }
     _limpaFormulario(){
         this._inputData.value = '';
--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,16 +1,16 @@
 class Negociacao {
-    constructor(data, quantidade,valor){
+    constructor(data, quantidade, valor){
         this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
-
+    
         Object.freeze(this);
     }
     get volume(){
-        return this._quantidade * this._valor;
+        return (this._quantidade * this._valor);
     }
     get data(){
-        return new Date(this._data);
+        return new Date(this._data.getTime());
     }
     get quantidade(){
         return this._quantidade;
