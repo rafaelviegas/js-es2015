3699a2766aef056eea717eea0bda1b9421b21462
Rafael Viegas <rafael.viegas@arpagus.com.br>
2016-11-24 10:49:42 -0200

    2. Gerenciando nossa conexão com o pattern Factory 3. Padronizando acesso aos dados com o pattern DAO

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -15,13 +15,39 @@
         this._mensagem = new Bind(
             new Mensagem(),new MensagemView($('#MensagemView')),
             'texto');
+        
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.listaTodos())
+            .then(negociacoes => 
+                negociacoes.forEach(negociacao => 
+                    this._listaNegociacoes.adiciona(negociacao)))
+            .catch(erro => {
+                console.log(erro);
+                this._mensagem.texto = erro;
+            });
     }
 
     adiciona(event){
-        event.preventDefault();   
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso!';
-        this._limpaFormulario();
+    
+        event.preventDefault();  
+
+        ConnectionFactory
+        .getConnection()
+            .then(connection => {
+                let negociacao = this._criaNegociacao();
+
+                new NegociacaoDao(connection)
+                    .adiciona(negociacao)
+                    .then(() => {
+                              this._listaNegociacoes.adiciona(negociacao);
+                              this._mensagem.texto = 'Negociação adicionada com sucesso!';
+                              this._limpaFormulario();
+                    })
+            })
+            .catch(erro => this._mensagem.texto = erro);
+
     }
 
     importaNegociacoes(){
@@ -33,9 +59,10 @@
             service.obterNegociacoesDaSemanaAnterior(),
             service.obterNegociacoesDaSemanaRetrasada()]
         ).then(negociacoes => {
-                negociacoes.
-                reduce((arrayAchatado, array) => arrayAchatado.concat(array),[])
-                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
+                negociacoes
+                .reduce((arrayAchatado, array) => arrayAchatado.concat(array),[])
+                .forEach(negociacao => 
+                this._listaNegociacoes.adiciona(negociacao))
                 this._mensagem.texto = 'Negociações importadas com sucesso';
                 })
             .catch(erro => this._mensagem.texto = erro);
@@ -52,15 +79,23 @@
     }
 
     apaga(){
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso!';
+
+        ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.apagaTodos())
+            .then(mensagem => {
+                this._mensagem.texto = mensagem;
+                this._listaNegociacoes.esvazia();
+            });
+           
     }
 
     _criaNegociacao(){
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value));
     }
 
     _limpaFormulario(){
